6efd5ea028cfaf1557e8028e10fd1be38de30a06
Mitch Kieffer <mitchkieffermain@gmail.com>
2022-02-22 20:22:22 -0600

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan')
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
  
 
 //Connect to DB
