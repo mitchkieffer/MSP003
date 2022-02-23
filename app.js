d7e2f3d0f6f32e4c7644edad8c69d38c84e8d73c
Mitch Kieffer <mitchkieffermain@gmail.com>
2022-02-23 11:08:48 -0600

    test

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ mongoose.connect('mongodb+srv://mkieffer:Aspenparks12@cluster0.t6jua.mongodb.net
 
 var indexRouter = require('./routes/index');
 var animalRouter = require('./routes/animal');
-var my_script = require('./public/javascripts/my_script');
 
 var app = express();
 
