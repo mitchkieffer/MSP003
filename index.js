685b6f1f88cbd54a52624750be44f7fbbdb65c6b
Mitch Kieffer <mitchkieffermain@gmail.com>
2022-02-22 19:09:26 -0600

    update

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const bodyParser = require('body-parser');
 const port = 3000;
  
 
-var indexRouter = require();
-var usersRouter = require();
+//var indexRouter = require();
+//var usersRouter = require();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,5 +36,5 @@ app.use('/test', getanimals);
 
 //app.use(express.static(__dirname));
 //app.use(express.json());
-//app.listen(process.env.PORT ?? 8080);
-app.listen(port, ()=>console.log('Application Started'));
+app.listen(process.env.PORT ?? 8080);
+//app.listen(port, ()=>console.log('Application Started'));
