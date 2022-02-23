a85e2bd8ff91a42682c7fc98b0cb401c261b727c
Mitch Kieffer <mitchkieffermain@gmail.com>
2022-02-22 19:37:59 -0600

    update

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan')
-const app = express();
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const port = 3000;
  
+
+//Connect to DB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://mkieffer:Aspenparks12@cluster0.t6jua.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 //var indexRouter = require();
 //var usersRouter = require();
+
+//Import Routes
+const getanimals = require('./getAnimals');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,23 +27,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use(bodyParser.json());
 
-//Connect to DB
-mongoose.connect('mongodb+srv://mkieffer:Aspenparks12@cluster0.t6jua.mongodb.net/test?retryWrites=true&w=majority',
-    { useNewUrlParser: true},
-    () => console.log("Connected")
-);
 
-//Import Routes
-const getanimals = require('./getAnimals');
 
 app.use('/test', getanimals);
 
 
-
-
-//app.use(express.static(__dirname));
-//app.use(express.json());
-app.use(process.env.PORT ?? 8080);
-//app.listen(port, ()=>console.log('Application Started'));
+module.exports = app;
