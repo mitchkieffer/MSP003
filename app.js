1e3b83e9e326b8824342d94b62c8e33808b2071e
Mitch Kieffer <mitchkieffermain@gmail.com>
2022-02-22 21:25:37 -0600

    update

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ mongoose.connect('mongodb+srv://mkieffer:Aspenparks12@cluster0.t6jua.mongodb.net
 });
 
 
-var indexRouter = require('./routes/animals');
+var indexRouter = require('./routes/index');
+var animalRouter = require('./routes/animals');
 
 var app = express();
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,22 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/animal', indexRouter);
+app.use('/', indexRouter);
+app.use('/animal', animalRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
